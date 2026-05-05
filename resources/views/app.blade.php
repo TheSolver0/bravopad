56e79df66f61b63df3e96d6897b889ed327815d1
TheSolver0 <lucfotso0@gmail.com>
2026-05-05 16:29:11 +0100

    changer le titre de la page

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -81,7 +81,8 @@
         @viteReactRefresh
         @vite(['resources/css/app.css', 'resources/js/app.tsx'])
         <x-inertia::head>
-            <title>{{ config('app.name', 'Laravel') }}</title>
+            <title>KudosPAD</title>
+            {{-- <title>{{ config('app.name', 'Laravel') }}</title> --}}
         </x-inertia::head>
     </head>
     <body class="font-sans antialiased">

363786e384f23b3928fe459e02d12d0262dd62ad
TheSolver0 <lucfotso0@gmail.com>
2026-06-01 20:34:02 +0100

    modification de la description et tentative d'elargissement de l'image cover d'apercu de lien changement logo laravle par mon propre logo

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -22,7 +22,7 @@
                 $evenementOg = [
                     'title'       => $ev->nom . ($evDateInfo ? ' · ' . $evDateInfo : ''),
                     'description' => $evDesc,
-                    'image'       => $ev->cover_image ? route('evenement.og-image', $ev->slug) : null,
+                    'image'       => $ev->cover_image ? url('/og-image/evenement/' . $ev->slug) : null,
                     'url'         => request()->url(),
                 ];
             }
@@ -133,10 +133,9 @@
             }
         </style>
 
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg">
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-        <link rel="icon" type="image/svg+xml" href="/favicon.svg">
-        <link rel="shortcut icon" href="/favicon.ico">
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
 
         <link rel="preconnect" href="https://fonts.bunny.net">
